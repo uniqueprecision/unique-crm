6b990151472f6fae8c3ce0022834912fee1fb127
uniqueprecision <uniqueprecisioncrm@gmail.com>
2026-03-06 01:48:29 +0530

    Delete backend/frontend directory

--- backend/frontend/login.js
+++
@@ -1,8 +0,0 @@
-function login() {
-  const role = document.getElementById("role").value;
-  if (!role) {
-    alert("Select role");
-    return;
-  }
-  window.location.href = "/" + role + ".html";
-}
