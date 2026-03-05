4c4337c1569a8a86e21967b8ce8fe498613c875b
uniqueprecision <uniqueprecisioncrm@gmail.com>
2026-03-06 02:21:22 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- backend/server.js
+++ backend/server.js
@@ -1424,7 +1424,21 @@ qcRows.forEach(row => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+app.use(express.static(path.join(__dirname,"../frontend")))
+
+app.get("/",(req,res)=>{
+res.sendFile(path.join(__dirname,"../frontend","login.html"))
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+console.log("Server running")
+})
+
 
